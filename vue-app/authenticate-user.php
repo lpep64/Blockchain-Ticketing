c5c13782690f9faa5cdca7f21860651821c404b3
AntTye <w21.tyler@gmail.com>
2024-11-20 04:22:07 -0500

    IT Help, Move Into Correct Directory

--- vue-app/authenticate-user.php
+++ vue-app/authenticate-user.php
@@ -2,13 +2,24 @@
 session_start();
 require_once 'vendor/autoload.php';
 
-phpCAS::client(CAS_VERSION_2_0, 'login.uconn.edu', 443, '/cas', 'https://blockchainticketing.ue.r.appspot.com/authenticate-user');
+phpCAS::client(CAS_VERSION_2_0, 'cas-msb-dev.grove.ad.uconn.edu', 443, '/cas', 'https://blockchainticketing.ue.r.appspot.com/authenticate-user.php');
 phpCAS::setNoCasServerValidation();
 
 phpCAS::forceAuthentication();
 
-$_SESSION['netid'] = phpCAS::getUser();
+// Get the authenticated user's NetID
+$netid = phpCAS::getUser();
+?>
 
-// Redirect back to the Vue app
-header('Location: https://blockchainticketing.ue.r.appspot.com/');
-exit();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <title>Authenticated User</title>
+</head>
+<body>
+   <h1>Welcome, <?php echo htmlspecialchars($netid); ?>!</h1>
+   <p><a href="logout.php">Logout</a></p>
+</body>
+</html>
+
