7de01b6ac99788b4307210677612e49fa9bb4711
Tyler <w21.tyler@uconn.edu>
2024-11-22 19:18:21 -0500

    Vue routes, yaml change

--- vue-app/authenticate-user.php
+++ vue-app/authenticate-user.php
@@ -4,22 +4,12 @@ require_once 'vendor/autoload.php';
 
 phpCAS::client(CAS_VERSION_2_0, 'cas-msb-dev.grove.ad.uconn.edu', 443, '/cas', 'https://blockchainticketing.ue.r.appspot.com/authenticate-user.php');
 phpCAS::setNoCasServerValidation();
-
 phpCAS::forceAuthentication();
 
 // Get the authenticated user's NetID
 $netid = phpCAS::getUser();
+$_SESSION["netID"] = $netid;
+
+header("Location: /");
+exit;
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <title>Authenticated User</title>
-</head>
-<body>
-   <h1>Welcome, <?php echo htmlspecialchars($netid); ?>!</h1>
-   <p><a href="logout.php">Logout</a></p>
-</body>
-</html>
-
