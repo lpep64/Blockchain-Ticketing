4075dbddee6b42e17720ec1cb20f56b9d73536df
Luke Pepin <112979003+lpep64@users.noreply.github.com>
2024-12-09 18:05:35 -0500

    Login/Logout works on website (#7)
    Co-authored-by: Tyler <w21.tyler@uconn.edu>

---
+++ vue-app/src/components/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container flex flex-col items-center mt-10">
+    <h1 class="text-3xl mb-3">Sorry, this page does not exist!</h1>
+    <router-link class="btn btn-light-green" :to="{ name: 'home' }">
+      Back To Home
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
