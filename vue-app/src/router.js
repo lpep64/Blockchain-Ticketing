800c6881507a7fca5d3615f4306089df512de21e
Matt Foley <149637862+SMattFoley@users.noreply.github.com>
2025-03-03 10:55:38 -0500

    Update router.js

--- vue-app/src/router.js
+++ vue-app/src/router.js
@@ -1,6 +1,5 @@
 // src/router.js
 import { createRouter, createWebHistory } from 'vue-router'
-<<<<<<< HEAD
 
 import home from './components/Index.vue'
 import Events from './components/Events.vue'
@@ -10,16 +9,6 @@ const routes = [
     { path: '/', component: home},
     { path: '/events', component: Events },
     { path: '/wallet', component: Wallet },
-
-=======
-import GenerateTicket from './components/GenerateTicket.vue'
-import TransferTicket from './components/TransferTicket.vue'
-
-const routes = [
-    { path: '/', component: GenerateTicket },
-    { path: '/generate-ticket', component: GenerateTicket },
-    { path: '/transfer-ticket', component: TransferTicket },
->>>>>>> origin/Private-network
     { path: '/:pathMatch(.*)*',
         name: 'not-found',
         component: () => import('./components/NotFound.vue'),
